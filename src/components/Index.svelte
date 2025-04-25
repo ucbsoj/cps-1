2f6918ec33398c1b4e43b540f724e7bb5ff97ffa
Michelle McGhee <michmac202@gmail.com>
2025-04-24 17:59:54 -0700

    working spinner for 2 or 3 options

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -2,10 +2,14 @@
 	import Hero from "$components/Hero.svelte";
 	import Main from "$components/Main.svelte";
 	import Footer from "$components/Footer.svelte";
+	import Decision from "$components/Decision.svelte";
+
+	let choices = ["Option 1", "Option 2", "Option 3"];
+	let outcomes = [
+		"This is the outcome of option 1. It should be a long paragraph that describes what happens when the user chooses this option. It can be as long as you want, but it should be at least a few sentences long.",
+		"This is the outcome of option 2. It should be a long paragraph that describes what happens when the user chooses this option. It can be as long as you want, but it should be at least a few sentences long.",
+		"This is the outcome of option 3. It should be a long paragraph that describes what happens when the user chooses this option. It can be as long as you want, but it should be at least a few sentences long."
+	];
 </script>
 
-<Hero />
-<Main />
-<Footer />
-
-<p>Text testing 123 if i start messing around is this gonna properly show?</p>
+<Decision decisionText="Choose one of these options." {choices} {outcomes} />
